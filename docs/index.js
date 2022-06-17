2cd1079ebcebb8e27cf689759afeb562f05f168e
pppiro <84230554+pppiro@users.noreply.github.com>
2022-06-17 12:20:05 +0900

    記事途中

--- docs/index.js
+++ docs/index.js
@@ -144,7 +144,7 @@ function initMap() {
         markerOptions[i] = {
             title: '',
             map: map,
-            position: new google.maps.LatLng(gallerydata2[i].lat, gallerydata2[i].lon),
+            position: new google.maps.LatLng(gallerydata2[gallerydata[i].venueId-1].lat, gallerydata2[gallerydata[i].venueId-1].lon),
             icon:'images/pin.png', //アイコンのURL
         }
 
@@ -159,12 +159,12 @@ function initMap() {
 
         //ホバー時のインフォの内容の設定
         var ContentStr = [];
-        ContentStr = `<p class="hover"><img src="${gallerydata2[i].photo}" height="130px" class="img"></a></p><br>
+        ContentStr = `<p class="hover"><img src="${gallerydata2[gallerydata[i].venueId-1].photo}" height="130px" class="img"></a></p><br>
         <p class="venue">${gallerydata[i].venue}<p><br>
         <p><button type="button" onclick="moveURL(${i})" class="hpButton">会場HP</button><p>`; //ボタンを押すとURLのページに遷移
         iwopts[i] = {
             content: ContentStr, 
-            position: new google.maps.LatLng(gallerydata2[i].lat, gallerydata2[i].lon),
+            position: new google.maps.LatLng(gallerydata2[gallerydata[i].venueId-1].lat, gallerydata2[gallerydata[i].venueId-1].lon),
         }
 
         //インフォの設定
@@ -216,7 +216,7 @@ function initMap() {
             //document.getElementById(i).style.height = "300px";
 
             //クリックしたmarkerが中心になるようにパン
-            map.panTo(new google.maps.LatLng(gallerydata2[i].lat, gallerydata2[i].lon));
+            map.panTo(new google.maps.LatLng(gallerydata2[gallerydata[i].venueId-1].lat, gallerydata2[gallerydata[i].venueId-1].lon));
 
             //離れすぎていたらズーム
             var currentZoom = map.getZoom();
@@ -254,7 +254,7 @@ function galleryMenuEdit()
         //クリック時イベント
         new_element.addEventListener('click', function(){
             //クリックした展示と対応したギャラリーのmarkerが中心になるようにパン
-            map.panTo(new google.maps.LatLng(gallerydata2[elementID].lat, gallerydata2[elementID].lon));
+            map.panTo(new google.maps.LatLng(gallerydata2[gallerydata[elementID].venueId-1].lat, gallerydata2[gallerydata[elementID].venueId-1].lon));
 
             //離れすぎていたらズーム
             var currentZoom = map.getZoom();
@@ -333,12 +333,28 @@ function popUp(i)
 
     document.getElementById("thumbnail").innerHTML = `<img src="${gallerydata[i].thumbnail}" class="photo">`
     document.getElementById("title").innerHTML = gallerydata[i].title;
-    //document.getElementById("date").innerHTML = `${gallerydata[i].startDate} - ${gallerydata[i].endDate}`;
+    document.getElementById("venue").innerHTML = gallerydata[i].venue;
     document.getElementById("admission").innerHTML = gallerydata[i].admission;
-    document.getElementById("url").innerHTML = `<a href="${gallerydata[i].url}"></a>`;
-    document.getElementById("openingtime").innerHTML = gallerydata2[i].openingTime;
-    document.getElementById("access").innerHTML = gallerydata2[i].access;
-    document.getElementById("article").innerHTML = "";
+    document.getElementById("url").innerHTML = `<a href="${gallerydata[i].url}"><font color="white">${gallerydata[i].url}</a>`;
+    document.getElementById("openingtime").innerHTML = gallerydata2[gallerydata[i].venueId-1].openingTime;
+    document.getElementById("access").innerHTML = gallerydata2[gallerydata[i].venueId-1].station + " " + gallerydata2[gallerydata[i].venueId-1].access;
+
+    if (i === 7)
+    {
+        document.getElementById("article").style.visibility = "visible";
+        document.getElementById("article").innerHTML = "もののイメージを積み重ね、時間という抽象的な概念を１つの画面上に表現し過去、未来、今の時間に実体を与える試みをしている大橋麻里子のこの作品展は、タイトルである「cross story」にある通り、時間軸によって自分が見たり感じたりした記録を一つの作品の画面上に存在させ、それぞれの物語を交差させる意図を持ちます。それにより私たちは走馬灯的に作家自身の記憶に思いを馳せることができ、元の時間軸から切り離されたモチーフ達は新しい１つの作品の枠組みの中で交わり合い新たなストーリーを作り上げていくのではないでしょうか。";
+    }
+    else
+    {
+        document.getElementById("article").innerHTML = "";
+        document.getElementById("article").style.visibility = "hidden";
+    }
+    /*
+    if (i === )
+    {
+        document.getElementById("article").innerHTML = "「TAMAのアートは百花繚乱」という展示は、多摩信用金庫の所蔵品から構成されるコレクション展です。<br>人間国宝にも選ばれた陶工藤本能道、日本画家の佐藤多持、篆刻家の本田谷庵など、多摩を中心に創作活動をした作家たちの作品を並べ、多摩の文化の豊かさを紹介しています。<br>美術館の中には資料室が併設されており、モネ・ダリなど著名な海外作家についての資料集を読めるだけではなく、多摩に関する美術、芸術に関する資料を探すことができます。<br>たましん歴史美術館は多摩信用金庫の６階にあることもあってか、あまり人がおらず静かなので落ち着いて鑑賞することができ、資料室でもゆっくり本を読むことができました。<br>では、この展示でコレクションされている作品の中から佐藤多持の作品について紹介したいと思います。<br>佐藤多持は1919年に東京府多摩郡国分寺町に生まれ、真言宗観音寺の次男として生まれ、1937年に東京美術学校日本画科に入学しました。戦後間もないころに尾瀬へのスケッチ旅行で水芭蕉に出会って以来、一貫して水芭蕉をモチーフとして描き続けていたが、次第にその作風は具体的なものから半球形や垂直線、水平線などの抽象的なパターンによる構成も経て1960年代から大きな墨線の円弧を使用した抽象画である“水芭蕉曼荼羅“シリーズへと移行していきました。<br>独特な水芭蕉の曲線美と宗教観と宇宙、世界の在り方を表現する曼荼羅を組み合わせ芸術的な抽象画として表現した水芭蕉曼荼羅は、曼荼羅という名前の通り彼の生まれた寺である真言宗観音寺の宗教観や世界観が受け継がれていると考えられます。仏や菩薩など諸仏諸尊の集会する楼閣を模式的に上下左右対称的に描かれるとされる曼荼羅とは少し違う不規則性を持った形で描かれる水芭蕉曼荼羅ですが、これは佐藤多持が自分自身で曼荼羅を解釈し、自分が感動した水芭蕉という形で曼荼羅を表現する佐藤多持の世界観と宗教観を窺い知ることができます。不規則性を持った曲線や円弧を絵画として描くうえで、曼荼羅のようなシンメトリックな気持ちよさを水芭蕉の曲線に見出したのではないでしょうか。";
+    }
+    */
 }
 
 //テスト用
